cc592bba521636a3f6d65b1bb3ca19c3079b8f4a
jensderkennts <p1ggyth3f1sh@gmail.com>
2022-11-10 19:22:28 +0100

    Update Homework_2.py
    big steppa/fast final

--- 02/Homework_2.py
+++ 02/Homework_2.py
@@ -1,67 +1,128 @@
-import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.random.uniform(low=0,high=1,size=100)
-t = [i**3 - i**2 for i in x]
-t = np.array(t)
 
-plt.plot(t)
-plt.plot(x)
-plt.show()
+x_vals = np.random.uniform(low=0,high=1,size=100)
+print(x_vals.shape)
+t = [i**3 - i**2 for i in x_vals]
+t_vals = np.array(t)
+
+plt.scatter(x_vals,t)
+# plt.show()
+
+
+def ReLU(x):
+    return np.maximum(0,x)
 
 def ReLU_derivative(x):
     # CURRENTLY LINEAR. TURN INTO BOOLEAN
-    return x 
+    return (x>0).astype(np.float64)
 
-class Layer(object):
-
-    def __init__(self, n_inputs, n_units, layer_input, layer_preactivation = None, layer_activation = None, layer_index = 0):
-        # self.W = tf.Variable(tf.random.uniform(shape=(n_inputs,n_units)))   # random weight matrix
-        self.W = tf.Variable(tf.ones(shape=(n_inputs,n_units)))
-        self.b = tf.Variable(tf.zeros(n_units))                               # biases of 0
-        self.layer_input = tf.reshape(layer_input, shape = (1,n_inputs))      # reshape input for vector matrix multiplication
-        self.layer_preactivation = layer_preactivation                      
-        self.layer_activation = layer_activation
+def mse(target, output): 
+    return np.mean((target -output) ** 2)
 
 
-    def forward_step(self):
-        self.layer_preactivation = tf.matmul(self.layer_input, self.W) + self.b     # multiply inputs with weights, add bias
-        self.layer_activation = tf.nn.relu(self.layer_preactivation)                # apply activation function
+def mse_derivative(target, output):
+    return output - target
+
+def liner(x):
+
+    return x
+
+def linar_derivation(x):
+    return 1
+
+class Layer():
+    def __init__(self, n_inputs, n_units, act_func = ReLU, der_act_func = ReLU_derivative, learning_rate = 0.002, variance = 0.1) -> None:
+        
+        self.weights = np.random.normal(scale = variance, size = (n_inputs + 1,n_units))
+
+        self.act_func = act_func
+        self.der_act_func = der_act_func
+        self.lear = learning_rate
+
+        self.layer_input = None
+        self.layer_preactivation = None
+        self.layer_activation = None
+
+
+    def __call__(self, inputs:np.array) :
+        self.layer_input = np.insert(inputs,0,1) # bias in the weights
+
+        self.layer_preactivation = self.layer_input.T @ self.weights
+        self.layer_activation = self.act_func(self.layer_preactivation)
         return self.layer_activation
 
-    def backward_step(self):
-        target = np.array([1,1,1,1]).astype(np.float32)
-        learning_rate = 0.05
-        # ADD POSSIBILITY TO DIFFERENTIATE BETWEEN OUTPUT LAYER AND HIDDEN LAYER
-        bias_gradient = ReLU_derivative(self.layer_preactivation) * (self.layer_activation - target)
-        weight_gradient = self.layer_input * bias_gradient
-        input_gradient = bias_gradient @ tf.transpose(self.W)
+    def backward_step(self, delta):
 
-        self.W = self.W - learning_rate * weight_gradient
-        self.b = self.b - learning_rate * bias_gradient
+        grad = self.der_act_func(self.layer_preactivation) * delta
+
+        grad_weights = self.layer_input.reshape(-1,1) * grad
+
+        delta = (self.weights * grad).sum(axis=1)
+        delta = np.delete(delta,0) # remove bias from weights
+
+        self.weights -= grad_weights * self.lear
+
+        return delta
+
+
 
 class MLP(object):
-    def __init__ (self, layer_units, network_input, layer_list = np.array([])):
-        n_layers = len(layer_units)
-        self.layer_list = layer_list
+    def __init__ (self, n_inputs, layers : list ,lr = 0.0002):
+        
+        self.layer_list = []
+        self.n_inputs = n_inputs
 
-        for i in range(n_layers):
-            if i == 0:              # first layer
-                previous_layer = Layer(n_inputs=layer_units[i], n_units=layer_units[i], layer_input = network_input, layer_index = i)
-                layer_list = np.append(layer_list,previous_layer)
+        last = n_inputs
+        for n_units, act_func, dev_act_func in layers:
 
-            else:
-                previous_layer = Layer(n_inputs = layer_units[i-1], n_units=layer_units[i], layer_input = previous_layer.forward_step(), layer_index = i)
-                layer_list = np.append(layer_list,previous_layer)
+            self.layer_list.append(
+                Layer(last,n_units,act_func,dev_act_func,learning_rate=lr)
+            )
+            last = n_units
+
+    def __call__(self, x):
+        
+        for layer in self.layer_list:
+            x = layer(x)
+
+        return x
+
+    def backward(self,delta):
+
+        for layer in reversed(self.layer_list):
+            delta = layer.backward_step(delta)
+
+        
 
 
-    def forward_step():
-        pass
 
-inputs = (tf.Variable((1,1,1,1),dtype=tf.float32))
-deutschland = Layer(n_inputs=4, n_units=4, layer_input=inputs)
-print(deutschland.W)
-print(deutschland.forward_step())
-deutschland.backward_step()
-deutschland.W,deutschland.b
+layers = [(50,ReLU,ReLU_derivative),(90,ReLU,ReLU_derivative),(1,liner,linar_derivation)]
+m = MLP(1,layers)
+
+
+
+def step(m, x_vals, y_vals,loss,dev_loss):
+    
+    mse = []
+
+    for x,y in zip(x_vals,y_vals):
+
+        pred = m(x.reshape(1,1))
+        mse.append(loss(pred,y))
+
+        delta = dev_loss(y,pred)
+
+        m.backward(delta)
+
+    return np.mean(mse)
+
+learning = []
+for i in range(10000):
+    learning.append( step(m,x_vals,t_vals,mse,mse_derivative))
+    
+plt.scatter(x_vals,[m(x.reshape(1,1)) for x in x_vals])
+# plt.plot(learning[10: ])
+plt.show()
+print(learning)
