5ae7f69a425164b14a6eee6e5d306ef0c4d5663b
Khang <duyknguyen@uni-osnabrueck.de>
2022-11-12 22:26:56 +0100

    homework final
    replaced old stuff with final notebook version

--- 02/mse_mse_prime.py
+++
@@ -1,9 +0,0 @@
-"""Loss function and it's derivative"""
-
-import numpy as np
-
-def mse(y_pred, y_true):
-    return np.power(y_pred-y_true, 2) / 2
-
-def mse_prime(y_pred, y_true):
-    return y_true - y_pred
--- 02/relu_relu_prime.py
+++
@@ -1,7 +0,0 @@
-"""ReLU activation function and it's derivative"""
-
-def relu(x):
-    return x * (x > 0)
-
-def relu_prime(x):
-    return 1 * (x > 0)
